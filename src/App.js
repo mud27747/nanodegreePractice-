ad0d51ec9411f6364f2450525052f81b88664644
Mudassir Gadit <gaditmudassir@gmail.com>
2020-04-22 14:24:48 +0000

    files for upload

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import FavoriteMovies from './FavoriteMovies';
+import { profiles, users, movies } from './data.js';
 
 class App extends Component {
-  state = {
-    echo: ""
-  };
-  handleChange = e => {
-    const echo = e.target.value;
-    this.setState(prevState => ({
-      echo: echo
-    }));
-  };
   render() {
-    const { echo } = this.state;
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
           <img src="logo.svg" className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
-          <p>Exercise 1 - Controlled Component</p>
         </header>
-        <main className="App-main">
-          <div className="container">
-            <input
-              type="text"
-              placeholder="Say Something"
-              value={echo}
-              onChange={this.handleChange}
-            />
-            <p className="echo">Echo:</p>
-            {echo === "" ? (
-              <p>This should mirror the text you typed into the input field.</p>
-            ) : (
-              <p>{echo}</p>
-            )}
-          </div>
-        </main>
+        <h2>Favorite Movies</h2>
+        <FavoriteMovies profiles={profiles} users={users} movies={movies} />
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const profiles = [
+  {
+    id: 1,
+    userID: '1',
+    favoriteMovieID: '1',
+  },
+  // more records...
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: 'Jane Cruz',
+    userName: 'coder',
+  },
+  // more records...
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: 'Planet Earth 1',
+  },
+  // more records...
+};
+
+export {profiles, users, movies};
